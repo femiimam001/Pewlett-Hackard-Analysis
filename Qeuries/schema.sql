57a0878380d3f40da446e0073440f71be26cb55a
femiimam001 <olufemi.imam@gmail.com>
2022-03-16 04:57:57 -0400

    adding new changes to queries file

--- Qeuries/schema.sql
+++ Qeuries/schema.sql
@@ -64,3 +64,22 @@ FROM current_emp as ce
 LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no;
+
+-- The first requested list is general employee information
+-- Joining more than 2 tables together.
+SELECT
+	e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+-- INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
