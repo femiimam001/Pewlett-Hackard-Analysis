ac200818af8bf9d95de54d226b6a49fa3bdbc374
femiimam001 <olufemi.imam@gmail.com>
2022-03-16 19:39:53 -0400

    adding new file unique_titles.csv

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,5 +1,6 @@
 SELECT * FROM retirement_titles;		
--- Deliverable 1: The Number of Retiring Employees by Title	
+-- Deliverable 1: The Number of Retiring Employees by Title	1
+--  Use the INNER JOIN clause to join two tables on a primary key 
 SELECT 
 	e.emp_no,
 	e.first_name,
@@ -13,3 +14,35 @@ INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;	
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- SELECT DISTINCT ON (______) _____,
+-- ______,
+-- ______,
+-- ______
+
+-- INTO nameyourtable
+-- FROM _______
+-- WHERE _______
+-- ORDER BY _____, _____ DESC;
+-- Use Dictinct with Orderby to remove duplicate rows for part 2/Deliverable 1
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+
+
+-- Use Count to determine number of retirement-age employees by most recent job title 2/Deliverable 1
+SELECT * FROM retiring_titles;
+SELECT 
+	COUNT(ut.emp_no),Ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
